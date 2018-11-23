35b265d7ea9417083b4a626006462663694eb207
segi3channel <44390326+segi3channel@users.noreply.github.com>
2018-11-24 05:31:34 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanda kutif nya(")
+///Jika di hapus, maka bot tidak akan jalan
+///Ganti xxxxxxx dengan data watch&earn kalian yg sudah kalian ambil lewat ssl capture
+///tonton video tutorialnya sampai selesai, jgn jadi anak micin yg selalu pengen instan
+$ticket = "xxxxxxxxxxxxxxxxxx";
+$meid = "xxxxxxxxxxxxxxxxxx";
+$os="xxxxxxxxxxxxxxxxxx";
+$sign="xxxxxxxxxxxxxxxxxx";
