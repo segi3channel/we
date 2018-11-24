9a3e4f5d04297cc3e1228a537ec1c7d5dbe83df4
segi3channel <44390326+segi3channel@users.noreply.github.com>
2018-11-24 08:00:45 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
 ///Jangan hapus tanda kutif nya(")
 ///Jika di hapus, maka bot tidak akan jalan
-///Ganti xxxxxxx dengan data watch&earn kalian yg sudah kalian ambil lewat ssl capture
-///tonton video tutorialnya sampai selesai, jgn jadi anak micin yg selalu pengen instan
+///Ganti xxxxxxx dengan data watch&earn kalian 
+///yg sudah kalian ambil lewat ssl capture
+///tonton video tutorialnya sampai selesai, 
+///jgn jadi anak micin yg selalu pengen instan
+///
 $ticket = "xxxxxxxxxxxxxxxxxx";
 $meid = "xxxxxxxxxxxxxxxxxx";
 $os="xxxxxxxxxxxxxxxxxx";
